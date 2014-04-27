f06828a901d777aab78c9950f295efc07d953b9c
Ilari <ilari@example.com>
2014-04-27 16:24:32 +0300

    Some documentation to the scene stuff.

--- src/Scene/Scene.h
+++ src/Scene/Scene.h
@@ -1,3 +1,23 @@
+/*
+================================================================================
+
+Copyright (c) 2014 Ilari Paananen
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+================================================================================
+*/
 
 #ifndef __SCENE_H__
 #define __SCENE_H__
@@ -11,6 +31,8 @@
 class Model;
 class Shader;
 
+/// Scene class that holds the scene nodes.
+///
 class Scene
 {
     typedef std::unique_ptr<SceneNode> SceneNodePtr;
@@ -18,14 +40,19 @@ class Scene
     typedef std::vector<SceneObject *> ObjectList;
 
 public:
+    /// Creates the camera.
     Scene();
 
+    /// Returns the camera.
     SceneCamera *GetCamera();
+    /// Creates a renderable scene object node.
     SceneObject *CreateObject();
 
+    /// Renders the scene with given shader.
     void Render(Shader *shader);
 
 private:
+    /// Adds a scene node to the node list.
     void AddNode(SceneNode *node);
 
 private:
--- src/Scene/SceneNode.h
+++ src/Scene/SceneNode.h
@@ -1,3 +1,23 @@
+/*
+================================================================================
+
+Copyright (c) 2014 Ilari Paananen
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+================================================================================
+*/
 
 #ifndef __SCENENODE_H__
 #define __SCENENODE_H__
@@ -12,28 +32,44 @@
 class SceneNode
 {
 public:
+    /// Sets parent to 0.
     SceneNode();
+    /// Virtual destructor.
     virtual ~SceneNode();
 
+    /// Removes this from old parent and adds to the new parent.
     void SetParent(SceneNode *node);
+    /// Returns parent node.
     SceneNode *GetParent() const;
 
+    /// Adds a child node.
     void AddChild(SceneNode *node);
+    /// Removes a child node.
     void RemoveChild(SceneNode *node);
 
+    /// Sets local position.
     void SetPosition(const Vector3 &pos);
+    /// Gets local position.
     Vector3 GetPosition() const;
 
+    /// Sets local rotation.
     void SetRotation(const Matrix3 &rot);
+    /// Gets local rotation.
     Matrix3 GetRotation() const;
 
+    /// Gets the local rotation basis vectors.
     void GetBasisVectors(Vector3 &right, Vector3 &up, Vector3 &look) const;
 
+    /// Gets local transform matrix.
     const Matrix4 &GetLocalTransform() const;
+    /// Gets world transform matrix.
     const Matrix4 &GetWorldTransform();
+    /// Gets the inverse world transform matrix.
     Matrix4 GetInverseWorldTransform();
 
 private:
+    /// Sets world transform dirty flag.
+    /// Dirties children as well.
     void SetDirty();
 
 private:
